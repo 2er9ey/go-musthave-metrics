e26af4f58e2901fbd588ca7f675457ad43d057fe
Sergey N. Mamoilenko <msn@sibnet.ru>
2025-08-05 21:43:15 +0300

    test

---
+++ internal/handler/mainhandler_test.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandler(t *testing.T) {
+	type want struct {
+		contentType string
+		statusCode  int
+	}
+	tests := []struct {
+		name    string
+		request string
+		want    want
+	}{
+		{
+			name: "simple test #1",
+			want: want{
+				contentType: "text/plain",
+				statusCode:  200,
+			},
+			request: "/update/gauge/xxx/1.234",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, tt.request, nil)
+			request.Header.Set("Content-Type", "text/plain")
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(MainHandler)
+			h(w, request)
+
+			result := w.Result()
+
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
+
+			_, err := io.ReadAll(result.Body)
+			require.NoError(t, err)
+			err = result.Body.Close()
+			require.NoError(t, err)
+		})
+	}
+}
